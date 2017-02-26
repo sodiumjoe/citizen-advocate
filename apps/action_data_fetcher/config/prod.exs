c95d11b608afe86425b91e50e75144613491e062
Ted Hille <ted@heuristocratic.us>
2017-02-26 06:54:17 -0800

    Load in our configs with secrets (i.e. api keys)

--- apps/action_data_fetcher/config/prod.exs
+++ apps/action_data_fetcher/config/prod.exs
@@ -1,1 +1,6 @@
 use Mix.Config
+
+# NOTE: since this is "secret", it's not stored in GitHub
+#       if you need this, contact one of the contributors
+#       to this project
+import_config "prod.secret.exs"
